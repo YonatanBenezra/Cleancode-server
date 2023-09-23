0d9db975bbdf89f5f53eac778528438ef464e8c4
Rupok Koiry <koiry.rupok@gmail.com>
2023-09-23 20:50:09 +0600

    Quiz features added

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -3,6 +3,6 @@ const handlerFactory = require('./handlerFactory');
 
 exports.createQuiz = handlerFactory.createOne(Quiz);
 exports.updateQuiz = handlerFactory.updateOne(Quiz);
-exports.getAllQuizzes = handlerFactory.getAll(Quiz);
+exports.getAllQuizzes = handlerFactory.getAll(Quiz, { path: 'language' });
 exports.getQuiz = handlerFactory.getOne(Quiz);
 exports.deleteQuiz = handlerFactory.deleteOne(Quiz);
